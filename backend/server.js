78dc64b449cf3c2dff2a90293aa019009b58142d
Monika <monikas012005@gmail.com>
2026-06-22 13:09:44 +0530

    Fix server routes

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
+app.get('/', (req, res) => {
+  res.json({ message: 'HireAI Backend is running!' });
+});
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected successfully!'))
   .catch((err) => console.log('MongoDB connection error:', err));
